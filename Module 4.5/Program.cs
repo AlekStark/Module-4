fa7db62fe97007b38f08b909419ac16246cf271b
Aleksandr <alekstark@live.ru>
2026-01-18 17:19:47 +0300

    Add project files.

---
+++ Module 4.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace Module4
+{
+    class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+            Console.WriteLine("Введите имя");
+            User.Name = ReadLine();
+            WriteLine("Введите фамилию");
+            User.LastName = ReadLine();
+            WriteLine("Введите логин");
+            User.Login = ReadLine();
+        }
+    }
+}
