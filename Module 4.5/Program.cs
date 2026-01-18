a22128fc402eb33b2ff290ed0453ccf6cdf46e42
Aleksandr <alekstark@live.ru>
2026-01-18 17:34:11 +0300

    Update Module 4.5

--- Module 4.5/Program.cs
+++ Module 4.5/Program.cs
@@ -8,12 +8,35 @@ namespace Module4
         private static void Main(string[] args)
         {
             (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
-            Console.WriteLine("Введите имя");
-            User.Name = ReadLine();
-            WriteLine("Введите фамилию");
-            User.LastName = ReadLine();
-            WriteLine("Введите логин");
-            User.Login = ReadLine();
+            for (int k = 0; k < 3; k++)
+            {
+                WriteLine("Введите имя");
+                User.Name = ReadLine();
+                WriteLine("Введите фамилию");
+                User.LastName = ReadLine();
+                WriteLine("Введите логин");
+                User.Login = ReadLine();
+                User.LoginLength = User.Login.Length;
+                WriteLine("Есть ли у вас животные? Да или Нет");
+                var result = ReadLine();
+                if (result == "Y")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+                WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(ReadLine());
+                User.favcolors = new string[3];
+                WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = ReadLine();
+                }
+            }
+
         }
     }
 }
